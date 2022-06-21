a686aff5530907b57382d477302e708f7b2660ce
Jimmy Hartzell <jah259@cornell.edu>
2022-06-21 16:46:38 -0400

    clean up code

--- frontend/src/Frontend.hs
+++ frontend/src/Frontend.hs
@@ -3,24 +3,15 @@
 
 module Frontend where
 
-import Control.Monad
-import qualified Data.Text as T
-import qualified Data.Text.Encoding as T
-import Language.Javascript.JSaddle (eval, liftJSM)
-
 import Obelisk.Frontend
-import Obelisk.Configs
 import Obelisk.Route
 import Obelisk.Generated.Static
 
 import Reflex.Dom.Core
 
-import Common.Api
 import Common.Route
-import Common.WordTiles
 
 import Frontend.WordTiles (app)
-
 
 -- This runs in a monad that can be run on the client or the server.
 -- To run code in a pure client or pure server context, use one of the
--- frontend/src/Frontend/WordTiles.hs
+++ frontend/src/Frontend/WordTiles.hs
@@ -11,36 +11,25 @@ import           Data.FileEmbed
 import           Data.ByteString.Char8 (unpack)
 import qualified Data.Text as T
 import qualified Data.Set as Set
+import           Reflex.Class
 import           Reflex.Dom
 import           Control.Monad (forM_)
 import           Control.Monad.Fix (MonadFix)
 import           Common.WordTiles
 
-individualTile
-  :: ( DomBuilder t m
-     , PostBuild t m
-     )
-  => Dynamic t ScoredLetter
-  -> m ()
-individualTile dScoredLetter = do
-    let convertScore score = case score of
-            LetterScoreAllWrong -> "allwrong"
-            LetterScoreWrongPlace -> "wrongplace"
-            LetterScoreAllRight -> "allright"
-        dClass = convertScore . scoredLetterScore <$> dScoredLetter
-        dLetter = T.pack . (:[]) . scoredLetterLetter <$> dScoredLetter
-    elDynAttr "span" (("class" =:) <$> dClass) $ dynText dLetter
-
 gameRow
   :: ( DomBuilder t m
-     , PostBuild t m
      )
-  => Int
-  -> Dynamic t [ScoredLetter]
+  => [ScoredLetter]
   -> m ()
-gameRow wordLength dWord = do
-    el "div" $ forM_ [0..wordLength] $ \i -> do
-        individualTile $ (!! i) <$> dWord
+gameRow letters = el "div" $ do
+    forM_ letters $ \(ScoredLetter letter score) -> do
+        let cls = case score of
+                LetterScoreAllWrong -> "allwrong"
+                LetterScoreWrongPlace -> "wrongplace"
+                LetterScoreAllRight -> "allright"
+        elAttr "span" ("class" =: cls) $ el "b" $
+            text $ T.pack $ letter:[]
 
 gameDisplay
   :: ( DomBuilder t m
@@ -48,42 +37,50 @@ gameDisplay
      )
   => Dynamic t (Game, [GameMessage])
   -> m ()
-gameDisplay dGame = dyn_ $ renderGame <$> dGame where
-    renderGame (game, messages) = do
-        forM_ (gameGuesses game) $ \guess -> do
-            el "div" $ do
-                forM_ guess $ \(ScoredLetter letter score) -> do
-                    let cls = case score of
-                            LetterScoreAllWrong -> "allwrong"
-                            LetterScoreWrongPlace -> "wrongplace"
-                            LetterScoreAllRight -> "allright"
-                    elAttr "span" ("class" =: cls) $ el "b" $
-                        text $ T.pack $ letter:[]
-        forM_ messages $ \message -> do
-            el "p" $ text $ T.pack $ show message
+gameDisplay dGame = dyn_ $ ffor dGame $ \(game, messages) -> do
+    forM_ (gameGuesses game) $ \guess -> gameRow guess
+    forM_ messages $ \message -> el "p" $ text $ T.pack $ show message
 
 wordSet :: Int -> Set.Set String
 wordSet len = Set.fromList wordList where
     file = $(embedFile "/usr/share/dict/words")
     wordList = filter ((== len) . length) $ lines $ unpack file
 
+{-
+displayLetters
+  :: ( DomBuilder t m
+     , PostBuild t m
+     , MonadHold t m
+     , MonadFix m
+     , HasDocument m
+     )
+  => m ()
+displayLetters = do
+    doc <- askDocument
+    let newLetter = fmap show <$> domEvent Keydown doc
+    letters <- foldDyn (<>) "" newLetter
+    pure ()
+-}
+
 app
   :: ( DomBuilder t m
      , PostBuild t m
      , MonadHold t m
      , MonadFix m
+     , HasDocument m
      )
   => m ()
 app = do
     let
-        start = Game [] (wordSet 5) "PIETY"
+        start = Game [] (wordSet 5) "GLOAT"
         moveAll word (gm, _) = move word gm
     rec
         game <- foldDyn moveAll (start, []) newWord
         gameDisplay game
         newWord <- fmap (fmap T.unpack) $ el "div" $ do
-            inputText <- fmap value $ inputElement $ def
+            inputText <- fmap (fmap T.toUpper . value) $ inputElement $ def
             (submit, _) <- el' "button" $ text "Submit"
             let click = domEvent Click submit
             pure $ current inputText <@ click
+        displayLetters
     pure ()
